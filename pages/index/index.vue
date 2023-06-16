ca526b127c0163bbf546590ee78f1475c2bea3c5
zxjdzxb <1660154581@qq.com>
2023-06-16 17:28:54 +0800

    scroll-into-view

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,7 +10,18 @@
             </view>
         </u-navbar>
 
-        <cover-view class="contain" :style="'margin-top:' + navHeight + 'rpx'">
+        <view class="contain" :style="'margin-top:' + navHeight + 'rpx;'">
+            <scroll-view scroll-x="true" class="scroll-content" :scroll-into-view="scrollIntoIndex">
+                <view class="scroll-item" v-for="(item, index) in topBar" :key="index" :id="'top' + index" @tap="changeTab(index)">
+                    <text :class="topBarIndex === index ? 'f-active-color' : 'f-color'">{{ item.name }}</text>
+                </view>
+            </scroll-view>
+
+            <swiper @change="onChangeTab" :current="topBarIndex">
+                <swiper-item v-for="(item, index) in topBar" :key="index">
+                    <view>{{ item.name }}</view>
+                </swiper-item>
+            </swiper>
             <cover-view>自己封装</cover-view>
             <IndexSwiper></IndexSwiper>
             <Recommend></Recommend>
@@ -21,7 +32,7 @@
             <Cart cardTitle="热门推荐"></Cart>
             <Hot></Hot>
             <Shop></Shop>
-        </cover-view>
+        </view>
         <view class="uview">
             <Cart cardTitle="Uview"></Cart>
             <u-swiper :list="list1"></u-swiper>
@@ -36,14 +47,30 @@ export default {
         return {
             bgColor: '#ffffff',
             list1: ['https://cdn.uviewui.com/uview/swiper/swiper1.png', 'https://cdn.uviewui.com/uview/swiper/swiper2.png', 'https://cdn.uviewui.com/uview/swiper/swiper3.png'],
-            navHeight: 0
+            navHeight: 0,
+            //选中的索引
+            topBarIndex: 0,
+            //顶栏跟随的索引id值
+            scrollIntoIndex: 'top0',
+            //顶栏数据
+            topBar: [{ name: '推荐' }, { name: '运动户外' }, { name: '服饰内衣' }, { name: '鞋靴箱包' }, { name: '美妆个护' }, { name: '家居数码' }, { name: '食品母婴' }]
         };
     },
     onLoad() {
         this.navHeight = app.globalData.navHeight + 10;
-        console.log(this.navHeight);
     },
-    methods: {}
+    methods: {
+        changeTab(index) {
+            if (this.topBarIndex === index) {
+                return;
+            }
+            this.topBarIndex = index;
+            this.scrollIntoIndex = 'top' + index;
+        },
+        onChangeTab(e) {
+            this.changeTab(e.detail.current);
+        }
+    }
 };
 </script>
 
@@ -60,5 +87,20 @@ export default {
 }
 .u-nav-slot {
     display: flex;
+}
+.scroll-content {
+    width: 100%;
+    height: 80rpx;
+    white-space: nowrap;
+}
+.scroll-item {
+    display: inline-block;
+    padding: 10rpx 30rpx;
+    font-size: 32rpx;
+}
+.f-active-color {
+    color: #49bdfb;
+    padding: 10rpx 0;
+    border-bottom: 6rpx solid #49bdfb;
 }
 </style>
