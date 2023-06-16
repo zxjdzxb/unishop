1daedec0019710c44287327ff75af257a1c10f92
zxjdzxb <1660154581@qq.com>
2023-06-16 16:25:29 +0800

    navHeight

--- App.vue
+++ App.vue
@@ -1,8 +1,12 @@
 <script>
+import { systemInfo } from '@/common/system-info.js';
 export default {
+    globalData: {
+        navHeight: 0
+    },
     onLaunch: function () {
         console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！');
-        console.log('App Launch');
+        this.globalData.navHeight = systemInfo().navHeight + systemInfo().statusBarHeight; //头部导航栏总高度
     },
     onShow: function () {
         console.log('App Show');
@@ -19,11 +23,18 @@ export default {
 /* #ifndef APP-NVUE */
 @import '@/static/customicons.css';
 @import 'uview-ui/index.scss';
+
 // 设置整个项目的背景色
 page {
     background-color: #f5f5f5;
     margin: 0;
     padding: 0;
+}
+page::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    color: transparent;
+    display: none;
 }
 
 /* #endif */
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,20 +1,17 @@
 <template>
     <!-- HBuilderX 2.6.3+ 新增 page-meta, 详情：https://uniapp.dcloud.io/component/page-meta -->
     <view class="container">
-        <cover-view class="status_bar">
-            <u-sticky offset-top="200">
-                <!-- 这里是状态栏 -->
-                <u-navbar title="首页" :bgColor="bgColor">
-                    <view class="u-nav-slot" slot="left">
-                        <u-icon name="arrow-left" size="19"></u-icon>
-                        <u-line direction="column" :hairline="false" length="16" margin="0 8px"></u-line>
-                        <u-icon name="home" size="20"></u-icon>
-                    </view>
-                </u-navbar>
-            </u-sticky>
-        </cover-view>
-        <view>
-            <view>自己封装</view>
+        <!-- 这里是状态栏 -->
+        <u-navbar title="首页" :bgColor="bgColor">
+            <view class="u-nav-slot" slot="left">
+                <u-icon name="arrow-left" size="19"></u-icon>
+                <u-line direction="column" :hairline="false" length="16" margin="0 8px"></u-line>
+                <u-icon name="home" size="20"></u-icon>
+            </view>
+        </u-navbar>
+
+        <cover-view class="contain" :style="'margin-top:' + navHeight + 'rpx'">
+            <cover-view>自己封装</cover-view>
             <IndexSwiper></IndexSwiper>
             <Recommend></Recommend>
             <Cart cardTitle="猜你喜欢"></Cart>
@@ -24,7 +21,7 @@
             <Cart cardTitle="热门推荐"></Cart>
             <Hot></Hot>
             <Shop></Shop>
-        </view>
+        </cover-view>
         <view class="uview">
             <Cart cardTitle="Uview"></Cart>
             <u-swiper :list="list1"></u-swiper>
@@ -33,33 +30,18 @@
 </template>
 
 <script>
+const app = getApp();
 export default {
     data() {
         return {
             bgColor: '#ffffff',
-            list1: ['https://cdn.uviewui.com/uview/swiper/swiper1.png', 'https://cdn.uviewui.com/uview/swiper/swiper2.png', 'https://cdn.uviewui.com/uview/swiper/swiper3.png']
+            list1: ['https://cdn.uviewui.com/uview/swiper/swiper1.png', 'https://cdn.uviewui.com/uview/swiper/swiper2.png', 'https://cdn.uviewui.com/uview/swiper/swiper3.png'],
+            navHeight: 0
         };
     },
     onLoad() {
-        //暂时根据状态栏与导航栏高度，设置弹窗距离顶部距离，大佬们若有好的方法欢迎补充！！！
-        // uni.getSystemInfo({
-        //     success: (e) => {
-        //         let statusBar = 0; //状态栏高度
-        //         let customBar = 0; // 状态栏高度 + 导航栏高度
-        //         console.log('app-plus', e);
-        //         // #ifdef APP-PLUS
-        //         statusBar = e.statusBarHeight;
-        //         customBar = e.statusBarHeight + 44;
-        //         // #endif
-        //         // #ifdef H5
-        //         statusBar = 0;
-        //         customBar = e.statusBarHeight + 44;
-        //         // #endif
-        //         console.log('statusBar', statusBar);
-        //         console.log('customBar', customBar);
-        //         this.popupTop = customBar;
-        //     }
-        // });
+        this.navHeight = app.globalData.navHeight + 10;
+        console.log(this.navHeight);
     },
     methods: {}
 };
